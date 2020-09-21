d9b35fe0492d3043890a73a482c6b0817f44b834
Wsquads <luisdereyna74@gmail.com>
2020-09-21 23:12:33 +0200

    21/09

--- pray4rave/src/app/app.module.ts
+++ pray4rave/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ReleasesComponent } from './components/releases/releases.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    EventosComponent,
+    BlogComponent,
+    ReleasesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
